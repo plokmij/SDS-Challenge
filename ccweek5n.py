9cc52a7e92ff316bc968e1610f384eafbd1f2a48
plokmij <samfan@live.com>
2017-09-23 01:42:35 +0530

    Solved

---
+++ ccweek5n.py
@@ -0,0 +1,23 @@
+import re
+import sys
+regex = r"([' ']|['.']|[a-zA-Z])=[ ][0-9]*([ ]|[0-9]*|)*"
+f = open(sys.argv[1])
+test_str = f.read()
+
+matches = re.finditer(regex, test_str)
+mydic = {}
+
+for thing in list(matches):
+	plok = thing.group(0).split(' ')
+	if plok[0] == '':
+		for i in range(2,len(plok)):
+			mydic[int(plok[i])] = ' ' 
+	else:	
+		for i in range(1,len(plok)):
+			mydic[int(plok[i])] = plok[0][0]
+letlist = []
+for i in range(len(mydic)):
+	letlist.append(mydic[i])
+
+output = ''.join(letlist)
+print output
